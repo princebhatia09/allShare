33d29a4a827ab3104025ddbb1f42b811b568e0ab
princebhatia09 <34816200+princebhatia09@users.noreply.github.com>
2021-03-21 03:08:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.set("view engine","ejs")
 app.use(express.static("public"))
 app.use(express.json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 connectdb();
 // console.log(process.env.PORT)
